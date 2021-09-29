4071156f9ed31cbf6d740d0d74df1e7d9702a0ef
Komil <library.dll@mail.ru>
2021-09-29 21:44:12 +0500

    Add project files.

---
+++ ProblemsDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace ProblemsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = RemoveDuplicate("abbca");
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+
+        public static string RemoveDuplicate(string str)
+        {
+            var dict = new Dictionary<char, bool>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dict.TryGetValue(str[i],out bool a))
+                {
+                    dict[str[i]] = false;
+                }
+                else
+                {
+                    dict[str[i]] = true;
+                }
+            }
+
+            return new string (dict.Where(x=>x.Value).Select(x=>x.Key).ToArray());
+        }
+    }
+}
